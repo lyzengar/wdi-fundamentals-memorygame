f9612ea77b653bfd6b7f14bf06fe7d8df75b0261
Andrew Lyzenga <andrew@Macintosh.local>
2018-02-26 14:13:31 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 console.log("Up and running!");
 
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2){
+    if (cardsInPlay[0] === cardsInPlay[1]){
+        alert('You found a match!')
+    }
+    else{
+        alert('Sorry, try again.')
+    }
+};
+
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
